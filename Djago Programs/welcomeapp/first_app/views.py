2faad22e501c3456c31f5139a19d3d37b80bc12b
akash <akash300383@gmail.com>
2024-08-02 15:54:00 +0530

    done

--- Djago Programs/welcomeapp/first_app/views.py
+++ Djago Programs/welcomeapp/first_app/views.py
@@ -4,7 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Welcome to Django First App module")
+    #return HttpResponse("Welcome to Django First App module")
+    return HttpResponse(''' 
+            <div>
+                  <h2>Welcome to First_App sub module</h2>
+                <a href="about_us">AboutUs</a>|
+                <a href="contact_us">ContactUs</a>|
+                <a href="feedback">Feedback</a>|      
+            </div>
+    ''')
 
 def about_us(request):
     return HttpResponse("Welcome to Django First App module About Us ")
