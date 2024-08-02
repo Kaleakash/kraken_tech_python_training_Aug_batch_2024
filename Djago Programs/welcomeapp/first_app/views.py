6dd7974f7607ad10738e371bdbe11b0e28042f30
akash <akash300383@gmail.com>
2024-08-02 16:12:02 +0530

    done

--- Djago Programs/welcomeapp/first_app/views.py
+++ Djago Programs/welcomeapp/first_app/views.py
@@ -3,22 +3,32 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+# def index(request):
+#     #return HttpResponse("Welcome to Django First App module")
+#     return HttpResponse(''' 
+#             <div>
+#                   <h2>Welcome to First_App sub module</h2>
+#                 <a href="about_us">AboutUs</a>|
+#                 <a href="contact_us">ContactUs</a>|
+#                 <a href="feedback">Feedback</a>|      
+#             </div>
+#     ''')
+
 def index(request):
-    #return HttpResponse("Welcome to Django First App module")
-    return HttpResponse(''' 
-            <div>
-                  <h2>Welcome to First_App sub module</h2>
-                <a href="about_us">AboutUs</a>|
-                <a href="contact_us">ContactUs</a>|
-                <a href="feedback">Feedback</a>|      
-            </div>
-    ''')
+    return render(request,"index.html")
 
 def about_us(request):
-    return HttpResponse("Welcome to Django First App module About Us ")
+    #return HttpResponse("Welcome to Django First App module About Us ")
+    return render(request,"aboutus.html");
 
 def contact_us(request):
-    return HttpResponse("Welcome to Django First App module Contact us")
+    #return HttpResponse("Welcome to Django First App module Contact us")
+    return render(request,"contactus.html");
 
 def feedback(request):
-    return HttpResponse("Welcome to Django First App module Feedback")
+    #return HttpResponse("Welcome to Django First App module Feedback")
+    return render(request,"feedback.html");
+
+
+
+
