a03b55ec289b13076a9b6f8411bcd43a866216fc
akash <akash300383@gmail.com>
2024-08-09 16:43:43 +0530

    done

--- Djago Programs/welcomeapp/product_app/urls.py
+++ Djago Programs/welcomeapp/product_app/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns=[
     path("",views.index),
     path("add_product",views.add_product),
+    path("add_product_info",views.add_product_info),
     path("delete_product",views.delete_product),
     path("update_product",views.update_product),
     path("view_product",views.view_product),
--- Djago Programs/welcomeapp/product_app/views.py
+++ Djago Programs/welcomeapp/product_app/views.py
@@ -2,11 +2,36 @@ from django.shortcuts import render
 
 # Create your views here.
 
+list_of_products=[];
+
 def index(request):
     return render(request,"index.html")
 
 def add_product(request):
     return render(request,"add_product.html")
+
+def add_product_info(request):
+    pid = int(request.GET.get("pid"))
+    pname = request.GET.get("pname");
+    price = float(request.GET.get("price"))
+
+    product = {"pid":pid,"pname":pname,"price":price}
+
+    print(product);
+    
+    global flag
+    flag=0
+    for product in list_of_products:
+        if product.get("pid")==pid:
+            flag=1;
+    
+    if flag==0:
+        list_of_products.append(product);
+        context={"result":"Product details added successfully"} 
+    else:
+        context={"result":"Product id must be unique"} 
+    
+    return render(request,"add_product.html",context)
 
 def delete_product(request):
     return render(request,"delete_product.html")
