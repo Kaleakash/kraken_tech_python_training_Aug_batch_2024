fed3931d8521ac6455c4ff792dd24a7628cace70
akash <akash300383@gmail.com>
2024-08-16 14:45:33 +0530

    done

--- Djago Programs/welcomeapp/product_db/urls.py
+++ Djago Programs/welcomeapp/product_db/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns=[
     path("",views.index),
-    # path("add_product",views.add_product),
-    # path("add_product_info",views.add_product_info),
+    path("add_product",views.add_product),
+    path("add_product_info",views.add_product_info),
     # path("delete_product",views.delete_product),
     # path("delete_product_info",views.delete_product_info),
     # path("update_product",views.update_product),
--- Djago Programs/welcomeapp/product_db/views.py
+++ Djago Programs/welcomeapp/product_db/views.py
@@ -12,3 +12,26 @@ def view_product(request):
     print(products)
     context = {"products":products}
     return render(request,"view_product.html",context)
+
+
+def add_product(request):
+    return render(request,"add_product.html")
+
+def add_product_info(request):
+    pname=request.POST.get("pname")
+    price = request.POST.get("price")
+
+    p1=Product(product_name=pname,product_price=price)
+    p1.save()
+
+    context = {"result":"Product details stored successfully"}
+    return render(request,"add_product.html",context)
+
+
+
+
+
+
+
+
+
