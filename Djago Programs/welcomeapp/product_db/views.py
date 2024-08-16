d6c22ea65f107dc0021468632cf8f2220eaabe39
akash <akash300383@gmail.com>
2024-08-16 14:32:50 +0530

    done

--- Djago Programs/welcomeapp/product_db/views.py
+++ Djago Programs/welcomeapp/product_db/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from .models import Product;
 
 # Create your views here.
 
 def index(request):
+    print("index page")
     return render(request,"index.html")
 
 def view_product(request):
-
-    return render(request,"view_product.html")
+    products = Product.objects.values()     # connect with model to get the data.  
+    print(products)
+    context = {"products":products}
+    return render(request,"view_product.html",context)
