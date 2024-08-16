07f3399f5805d57deb5fe1588dda44035bd7fcf7
akash <akash300383@gmail.com>
2024-08-16 17:20:25 +0530

    done

--- Djago Programs/welcomeapp/api/urls.py
+++ Djago Programs/welcomeapp/api/urls.py
@@ -3,12 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    # path("",views.index),
-    # path("add_product",views.add_product),
-    # path("add_product_info",views.add_product_info),
-    # path("delete_product",views.delete_product),
-    # path("delete_product_info",views.delete_product_info),
-    # path("update_product",views.update_product),
-    # path("update_product_info",views.update_product_info),
-    # path("view_product",views.view_product),
+    path("employees/",views.employee_api),
+    path("departments/",views.department_api),
+    
 ]
--- Djago Programs/welcomeapp/api/views.py
+++ Djago Programs/welcomeapp/api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from .models import Department,Employee
+from .serializers import DepartmentSerializers,EmployeeSerializers
 # Create your views here.
+
+def department_api(request):
+    if request.method=="GET":
+        departments = Department.objects.all();     #department is python object or query  
+        department_serializer = DepartmentSerializers(departments,many=True)
+        return JsonResponse(department_serializer.data,safe=False)
+
+
+
+def employee_api(request):
+    if request.method=="GET":
+        employees = Employee.objects.all();
+        employee_serializer = EmployeeSerializers(employees,many=True)
+        return JsonResponse( employee_serializer.data,safe=False)
